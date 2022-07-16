13f93db40d0c3f86498805592aac5dfbe6aef662
Melany Rodríguez Montero <109432769+rmomelany@users.noreply.github.com>
2022-07-16 12:35:56 -0600

    Update streamlit_app.py
    Add more to the app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
+streamlit.text('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smootie')
+streamlit.text('Hard-Boiled' Free-Range Egg)
